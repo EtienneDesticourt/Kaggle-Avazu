cd31c83823adc758138b155328d8fbed014e9db4
EtienneDesticourt <etienne.desticourt@gmail.com>
2015-02-06 19:40:41 +0100

    VW: Tuned alpha, l1 reg and num features

--- VowpalWabbit/run.sh
+++ VowpalWabbit/run.sh
@@ -1,5 +1,6 @@
 
 
-vw train.vw -f avazu.model.vw --loss_function logistic -b22  -l 0.5 -c --passes 10 --holdout_after 32377422
+vw -q AB train.vw -f avazu.model.vw --loss_function logistic -b22  --l1 0.000000003375  -l 0.050 -c -k --passes 10 --holdout_after 32377422
 
-vw test.vw -t -i avazu.model.vw -p avazu.preds.txt -b22  -l 0.5
+#vw test.vw -t -i avazu.model.vw -p avazu.preds.txt -b22  -l 0.050 --l1 0.000000003375
+#roaming sheep
